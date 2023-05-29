74a5db8069bb13f89a511756ca7b5d800b6fd21e
yux-m <yuxmeng@seas.upenn.edu>
2023-05-29 16:19:49 -0400

    References

--- osquery/tables/system/windows/cpu_time.cpp
+++ osquery/tables/system/windows/cpu_time.cpp
@@ -7,16 +7,14 @@
  * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
  */
 
-/*
- *  Inspired from the psutil per_cpu_times function -
- *  https://github.com/giampaolo/psutil/blob/ec1d35e41c288248818388830f0e4f98536b93e4/psutil/_psutil_osx.c#L739
- */
-
-#include <windows.h>
-#include <psapi.h>
-
-#include <osquery/core/core.h>
+#include <osquery/core/system.h>
 #include <osquery/core/tables.h>
+#include <osquery/logger/logger.h>
+#include <osquery/sql/sql.h>
+#include <osquery/utils/conversions/tryto.h>
+#include "osquery/core/windows/wmi.h"
+
+#include <unordered_map>
 
 namespace osquery {
 namespace tables {
@@ -33,6 +31,8 @@ static inline long int ticks_to_usecs(int ticks) {
  * https://learn.microsoft.com/en-us/windows/win32/wmisdk/example--getting-wmi-data-from-the-local-computer
  * https://stackoverflow.com/questions/19756454/calculating-process-cpu-usage-from-process-totalprocessortime
  * https://wutils.com/wmi/root/cimv2/win32_perfformatteddata_perfos_processor/
+ * https://wutils.com/wmi/root/cimv2/win32_perfformatteddata_counters_processorinformation
+ * https://wutils.com/wmi/root/cimv2/win32_perfformatteddata_perfproc_process/ <= this one
  * https://www.philosophicalgeek.com/2009/01/03/determine-cpu-usage-of-current-process-c-and-c/
  * prev refs:
  * https://www.linuxhowtos.org/System/procstat.htm
@@ -49,65 +49,51 @@ static inline long int ticks_to_usecs(int ticks) {
 QueryData genCpuTime(QueryContext& context) {
   QueryData results;
 
-  // Get the list of process identifiers.
-  DWORD aProcesses[1024], cbNeeded, cProcesses;
-  unsigned int i;
-
-  // Calculate how many process identifiers were returned.
-  cProcesses = cbNeeded / sizeof(DWORD);
-
-  // Iterate through all processes.
-  for ( i = 0; i < cProcesses; i++ )
-  {
-    if( aProcesses[i] != 0 )
-    {
-      // Get a handle to the process.
-      HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, aProcesses[i] );
-      // get cpu time
-      FILETIME a,b,c,d;
-      if (GetProcessTimes(GetCurrentProcess(),&a,&b,&c,&d) != 0){
-        // kernal mode (system mode)
-        r["system"] = BIGINT((long int)(c.dwLowDateTime |
-            ((unsigned long long)c.dwHighDateTime << 32)) * 0.0000001);
-        // user mode
-        r["user"] = BIGINT((long int)(d.dwLowDateTime |
-            ((unsigned long long)d.dwHighDateTime << 32)) * 0.0000001);
-      }else{
-        //  Handle error
-        r["system"] = "0";
-        r["user"] = "0";
-      }
-    }
+  const Expected<WmiRequest, WmiError> wmiSystemReq_uptime =
+      WmiRequest::CreateWmiRequest("select ElapsedTime, Name from Win32_PerfFormattedData_PerfProc_Process");
+  if (!wmiSystemReq_uptime || wmiSystemReq_uptime->results().empty()) {
+    LOG(WARNING) << "Error retrieving information from WMI.";
+    return results;
   }
 
-  // kern_return_t ret =
-  //     host_processor_info(host,
-  //                         PROCESSOR_CPU_LOAD_INFO,
-  //                         &processor_count,
-  //                         reinterpret_cast<processor_info_t*>(&processor_times),
-  //                         &processor_msg_count);
+  const std::vector<WmiResultItem>& uptimeData = wmiSystemReq_uptime->results();
+  std::unordered_map<std::string, long long> uptimeMap;
+  for (const auto& data : uptimeData) {
+    std::string name;
+    data.GetString("Name", name);
+    long long temp = 0;
+    data.GetLongLong("ElapsedTime", temp);
+    uptimeMap[name] = temp;
+  }
 
-  // if (ret == KERN_SUCCESS) {
-  //   // Loop through the cores and add rows for each core.
-  //   for (unsigned int core = 0; core < processor_count; core++) {
-  //     Row r;
-  //     r["core"] = INTEGER(core);
-  //     r["user"] = BIGINT(
-  //         ticks_to_usecs(processor_times[core].cpu_ticks[CPU_STATE_USER]));
-  //     r["idle"] = BIGINT(
-  //         ticks_to_usecs(processor_times[core].cpu_ticks[CPU_STATE_IDLE]));
-  //     r["system"] = BIGINT(
-  //         ticks_to_usecs(processor_times[core].cpu_ticks[CPU_STATE_SYSTEM]));
-  //     r["nice"] = BIGINT(
-  //         ticks_to_usecs(processor_times[core].cpu_ticks[CPU_STATE_NICE]));
+  const Expected<WmiRequest, WmiError> wmiSystemReq =
+      WmiRequest::CreateWmiRequest("select Name, PercentUserTime, PercentPrivilegedTime, PercentIdleTime, PercentInterruptTime, PercentPriorityTime from Win32_PerfFormattedData_Counters_ProcessorInformation");
+  if (!wmiSystemReq || wmiSystemReq->results().empty()) {
+    LOG(WARNING) << "Error retrieving information from WMI.";
+    return results;
+  }
 
-  //     results.push_back(r);
-  //   }
-  //   vm_deallocate(
-  //       mach_task_self(),
-  //       reinterpret_cast<vm_address_t>(processor_times),
-  //       static_cast<vm_size_t>(processor_count * sizeof(*processor_times)));
-  // }
+  const std::vector<WmiResultItem>& wmiResults = wmiSystemReq->results();
+  for (const auto& data : wmiResults) {
+    std::string name;
+    data.GetString("Name", name);
+    r["core"] = name;
+    long long uptime = uptimeMap[name];
+    long percent = 0;
+    data.GetLongLong("PercentUserTime", percent);
+    r["user"] = BIGINT(percent / 100 * uptime);
+    data.GetLongLong("PercentPrivilegedTime", percent);
+    r["system"] = BIGINT(percent / 100 * uptime);
+    data.GetLongLong("PercentIdleTime", percent);
+    r["idle"] = BIGINT(percent / 100 * uptime);
+    long idle = percent;
+    data.GetLongLong("PercentInterruptTime", percent);
+    r["irq"] = BIGINT(percent / 100 * uptime);
+    data.GetLongLong("PercentPriorityTime", percent]);  // nice = 100 - priority - idle?
+    r["nice"] = BIGINT((100 - percent - idle) / 100 * uptime);
+    results.push_back(r);
+  }
+
   return results;
 }
 } // namespace tables
